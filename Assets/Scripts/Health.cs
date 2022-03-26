e2ecac20cf56262b5af29cb832850d3451b10a1e
najongjine <najongjin3@hotmail.com>
2022-03-26 17:39:48 +0900

    .

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,6 +7,14 @@ public class Health : MonoBehaviour
     [SerializeField] int health = 50;
     [SerializeField] ParticleSystem hitEffect;
 
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+
+    void Awake()
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
@@ -15,6 +23,7 @@ public class Health : MonoBehaviour
         {
             TakeDamage(damageDealer.GetDamage());
             PlayHitEffect();
+            ShakeCamera();
             damageDealer.Hit();
         }
     }
@@ -30,9 +39,19 @@ public class Health : MonoBehaviour
 
     void PlayHitEffect()
     {
-        ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(instance.gameObject, instance.main.duration);
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
     }
 
+    void ShakeCamera()
+    {
+        if (cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
+        }
+    }
 
 }
